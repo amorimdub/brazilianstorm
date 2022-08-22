a99624d4ea9399398d7c20679054fb6444e8d9d9
Felipe Silva <amorimdub@gmail.com>
2022-08-22 15:43:33 +0100

    fix: api

--- apps/api/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var server_1 = require("./server");
-var port = process.env.PORT || 5001;
-var server = (0, server_1.createServer)();
-server.listen(port, function () {
-    console.log("api running on ".concat(port));
-});
